05b57a906c3124f6097cb183454f973eb0b00b68
Alex Welch <me@alexwelch.com>
2014-04-04 13:08:54 -0600

    Place colored icons in styleguide. [#68905902]

--- app/helpers/ctl_base_ui/application_helper.rb
+++ app/helpers/ctl_base_ui/application_helper.rb
@@ -1,4 +1,7 @@
 module CtlBaseUi
   module ApplicationHelper
+    def all_icons
+      %w( plus minus x sprocket arrowloop checkmark document magnifying-glass pencil outbox grid envelope list folder menu house star thin-arrow-right thick-arrow-right magnifying-glass-plus magnifying-glass-minus)
+    end
   end
 end
