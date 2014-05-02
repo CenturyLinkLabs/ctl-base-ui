cce650613d5f5578034fa39ceb0cf3043b0b4006
Laura Frank <ljfrank@gmail.com>
2014-05-02 14:47:35 -0500

    Updating icon files and styleguide entry

--- app/helpers/ctl_base_ui/application_helper.rb
+++ app/helpers/ctl_base_ui/application_helper.rb
@@ -1,7 +1,10 @@
 module CtlBaseUi
   module ApplicationHelper
     def all_icons
-      %w( plus minus x sprocket arrowloop checkmark document magnifying-glass pencil outbox grid envelope list folder menu house star thin-arrow-right thick-arrow-right magnifying-glass-plus magnifying-glass-minus)
+      %w( plus minus x sprocket arrowloop checkmark document magnifying-glass
+        pencil outbox grid envelope list folder menu house star thin-arrow-right
+        thick-arrow-right magnifying-glass-plus magnifying-glass-minus comment-box
+        happy-face sad-face copy twitter linked-in facebook link docker-blocks )
     end
   end
 end
