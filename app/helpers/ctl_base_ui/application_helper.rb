1debb11d6cff51496312e1e6be6e1a31aa11273e
Alex Welch <me@alexwelch.com>
2014-05-15 15:26:41 -0600

    Add some icons. [#68664998]

--- app/helpers/ctl_base_ui/application_helper.rb
+++ app/helpers/ctl_base_ui/application_helper.rb
@@ -4,7 +4,9 @@ module CtlBaseUi
       %w( plus minus x sprocket arrowloop checkmark document magnifying-glass
         pencil outbox grid envelope list folder menu house star thin-arrow-right
         thick-arrow-right magnifying-glass-plus magnifying-glass-minus comment-box
-        happy-face sad-face copy twitter linked-in facebook link docker-blocks )
+        happy-face sad-face copy twitter linked-in facebook link docker-blocks
+        disks speedometer thin-arrow-down thick-arrow-down thin-arrow-left
+        thick-arrow-left thin-arrow-up thick-arrow-up )
     end
   end
 end
